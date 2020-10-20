6d868918c80a95e40cce4560d3ef9ad4b0f74ad5
MikhailMamchenkov <MishaMa7854>
2020-10-20 13:56:24 +0300

    Added some changes

--- src/sixthLesson/Animal.java
+++ src/sixthLesson/Animal.java
@@ -1,4 +1,34 @@
 package sixthLesson;
 
+import java.util.Random;
+
 public class Animal {
+
+    protected float maxRunLength;
+    protected float maxSwimLength;
+    protected float maxJumpHeight;
+
+    Animal(float maxRunLength, float maxSwimLength, float maxJumpHeight) {
+        this.maxRunLength = maxRunLength;
+        this.maxSwimLength = maxSwimLength;
+        this.maxJumpHeight = maxJumpHeight;
+    }
+
+    void run(float length) {
+        System.out.println("run " + isAvailableAction(maxRunLength, maxJumpHeight));
+    }
+
+    void swim(float length) {
+        System.out.println("swim " + isAvailableAction(maxRunLength, maxJumpHeight));
+    }
+
+    void jump(float height) {
+        System.out.println("jump " + isAvailableAction(maxRunLength, maxJumpHeight));
+    }
+
+    boolean isAvailableAction(float required, float expected) {
+        return required <= expected;
+    }
+
+
 }
---
+++ src/sixthLesson/Dog.java
@@ -0,0 +1,8 @@
+package sixthLesson;
+
+public class Dog extends Animal {
+
+    Dog(float maxRunLength, float maxSwimLength, float maxJumpHeight) {
+        super(maxRunLength, maxSwimLength, maxJumpHeight);
+    }
+}
